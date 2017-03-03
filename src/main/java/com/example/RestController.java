6bb8a408ef5dbbd515f53789120421b28aa1f010
cimpapps <catalincimpoeru24@gmail.com>
2017-03-03 16:20:15 +0200

    initial commit

---
+++ src/main/java/com/example/RestController.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RestController {
+
+	@Value("${rate}")
+	private String rate;
+	@Value("${lanecount}")
+	private String lanecount;
+	@Value("${tollstart}")
+	private String tollstart;
+
+	@Value("${constring}")
+	private String constring;
+
+	@RequestMapping("/rate")
+	public String getRate(Model m) {
+		m.addAttribute("rateamount", rate);
+		m.addAttribute("lanes", lanecount);
+		m.addAttribute("tollstart", tollstart);
+		m.addAttribute("constring", constring);
+		return "rateView";
+	}
+
+}
